f4ea263faa89938338219572a9de57c92a8cdb13
torvic <torvic02st96@gmail.com>
2023-07-11 00:16:57 -0500

    [6-appsettingconnection] Add App setting connection

--- AssignmentContext.cs
+++ AssignmentContext.cs
@@ -5,11 +5,18 @@ namespace projectef;
 
 public class AssignmentContext: DbContext
 {
+	protected readonly IConfiguration Configuration;
+	public AssignmentContext(IConfiguration configuration)
+	{
+			Configuration = configuration;
+	}
 	public DbSet<Category> Categories {get;set;}
 	public DbSet<Assignment> Assignments {get;set;}
 
-	public AssignmentContext(DbContextOptions<AssignmentContext> options): base(options) {}
+	// public AssignmentContext(DbContextOptions<AssignmentContext> options): base(options) {}
 
 	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseNpgsql(@"Host=localhost;Username=root;Password=root;Database=mydatabase");
+	{
+    optionsBuilder.UseNpgsql(Configuration.GetConnectionString("cnMyDatabase"));
+	}
 }
