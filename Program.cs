2bf0f25476c69a58a4dcfd7cf0ada6b443e91322
torvic <torvic02st96@gmail.com>
2023-07-10 23:13:56 -0500

    [4-dninmemory] Add db in memory connection

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projectef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AssignmentContext>(p => p.UseInMemoryDatabase("AssignmentDb"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconnection", async ([FromServices] AssignmentContext dbContext) =>
+{
+	dbContext.Database.EnsureCreated();
+	return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
