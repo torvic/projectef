0ccf44c37a6a4b7a2ca345fe457771b8dce61ae1
torvic <torvic02st96@gmail.com>
2023-07-10 22:09:30 -0500

    [1-modelos] Add Category & Task models

---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+namespace projectef.models;
+
+public class Category
+{
+	public Guid CategoryId { get; set; }
+	public string Name { get; set; }
+	public string Description { get; set; }
+
+	public virtual ICollection<Task> Tasks{ get; set; }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,24 @@
+namespace projectef.models;
+
+public class Task
+{
+	public Guid TaskId { get; set; }
+	public Guid CategoryId { get; set; }
+
+	public string Title { get; set; }
+
+	public string Description { get; set; }
+
+	public Priority TaskPriority { get; set; }
+
+	public DateTime CreationDate { get; set; }
+
+	public virtual Category Category { get; set; }
+}
+
+public enum Priority
+{
+	Low,
+	Medium,
+	High,
+}
