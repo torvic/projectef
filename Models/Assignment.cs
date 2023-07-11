0159c6db866e097e14a900195ec0d3813e04dd68
torvic <torvic02st96@gmail.com>
2023-07-10 23:05:04 -0500

    [3-mapattributes] Add data annotations to models

--- Models/Assignment.cs
+++ Models/Assignment.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace projectef.models;
 
 public class Assignment
 {
+	[Key]
 	public Guid AssignmentId { get; set; }
+
+	[ForeignKey("CategoryId")]
 	public Guid CategoryId { get; set; }
 
+	[Required]
+	[MaxLength(200)]
 	public string Title { get; set; }
 
 	public string Description { get; set; }
@@ -14,6 +22,9 @@ public class Assignment
 	public DateTime CreationDate { get; set; }
 
 	public virtual Category Category { get; set; }
+
+	[NotMapped]
+	public string Overview { get; set; }
 }
 
 public enum Priority
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace projectef.models;
 
 public class Category
 {
+	[Key]
 	public Guid CategoryId { get; set; }
+	
+	[Required]
+	[MaxLength(150)]
 	public string Name { get; set; }
 	public string Description { get; set; }
 
